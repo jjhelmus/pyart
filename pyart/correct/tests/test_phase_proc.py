4d29bfd435b6c0c9b3260f2f263b241f2046f24d
Jonathan Helmus <jjhelmus@gmail.com>
2013-07-15 14:20:53 -0500

    Test with skips (2)

--- pyart/correct/tests/test_phase_proc.py
+++ pyart/correct/tests/test_phase_proc.py
@@ -31,7 +31,7 @@ PATH = os.path.dirname(__file__)
 REFERENCE_RAYS_FILE = os.path.join(PATH, 'reference_rays.npz')
 
 
-@skipif(not cvxopt_available)
+@skipif(not glpk_available)
 def test_phase_proc_lp_glpk():
     radar, phidp, kdp = perform_phase_processing()
     ref = np.load(REFERENCE_RAYS_FILE)
@@ -41,7 +41,7 @@ def test_phase_proc_lp_glpk():
                   radar.fields['unf_dp_phase_shift']['data']) <= 0.01
 
 
-@skipif(not glpk_available)
+@skipif(not cvxopt_available)
 def test_phase_proc_lp_cvxopt():
     radar, phidp, kdp = perform_phase_processing('cvxopt')
     ref = np.load(REFERENCE_RAYS_FILE)
