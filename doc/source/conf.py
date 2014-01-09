23725f31dc0470963b7aca21a2b6f524794124fb
Jonathan Helmus <jjhelmus@gmail.com>
2014-01-09 16:32:52 -0600

    Real mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,30 +3,12 @@
 
 import sys, os, re
 
-import sys
 
 print "Whooooo"
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
-MOCK_MODULES = ['netCDF4', 'pyart.io._rsl_interface', 'pyproj']
+import mocks
+MOCK_MODULES = ['netCDF4', 'mpl_toolkits', 'pyproj']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 
 # Check Sphinx version
