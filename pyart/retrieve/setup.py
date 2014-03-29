ae4dd0082c365ac4054d60677d5f8c355c204e46
Kirk North <kirk.w.north@gmail.com>
2014-03-29 16:23:52 -0400

    MOD: retrieve setup.py
    Adding the f2py extensions to the setup. As of now this requires the pyf
    interface files for each of the corresponding Fortran routines.

--- pyart/retrieve/setup.py
+++ pyart/retrieve/setup.py
@@ -1,6 +1,5 @@
 from os.path import join
 
-
 def configuration(parent_package='', top_path=None):
     global config
     from numpy.distutils.misc_util import Configuration
@@ -9,6 +8,25 @@ def configuration(parent_package='', top_path=None):
 
     # Conditionally add Steiner echo classifier extension.
     config.add_extension('_echo_steiner', sources=[steiner_echo_gen_source])
+    config.add_extension('background',
+                         sources=['background.pyf','src/background.f90'],
+                         f2py_options=[])
+    config.add_extension('continuity',
+                         sources=['continuity.pyf','src/continuity.f90'],
+                         f2py_options=[])
+    config.add_extension('divergence',
+                         sources=['divergence.pyf','src/divergence.f90'],
+                         f2py_options=[])
+    config.add_extension('gradient',
+                         sources=['gradient.pyf','src/gradient.f90'],
+                         f2py_options=[])
+    config.add_extension('laplace',
+                         sources=['laplace.pyf','src/laplace.f90'],
+                         f2py_options=[])
+    config.add_extension('smooth',
+                         sources=['smooth.pyf','src/smooth.f90'],
+                         f2py_options=[])
+
     return config
 
 
