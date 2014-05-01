b806809abdaf0268c98bd13f2256208237a7e9c3
Jonathan Helmus <jjhelmus@gmail.com>
2014-05-01 14:27:06 -0500

    Before final refactor

--- pyart/correct/src/helpers.h
+++ pyart/correct/src/helpers.h
@@ -31,12 +31,6 @@
 		  **  to unfold the current bin. */
 #define STDTHRESH 0.8 /* Fraction of the Nyquist velocity to use as a standard
 		      **  deviation threshold when windowing. */
-#define LOWDBZ 0.0 /* All VR bins with DZ values less than LOWDBZ will be
-		     **  deleted. */
-#define HIGHDBZ 80.0 /* All bins with DZ values greater than HIGHDBZ will
-		     ** be deleted. */
-#define NODBZRMRV 0 /* If NODBZRMRV==1, all VR bins with DZ values missing
-		    ** will be deleted. */
 #define RM 0 /* If soundvolume is not available, remove cells left over after
 	     **  first pass. */
 #define PASS2 1 /* Flag specifying the use of a second pass using only the
@@ -45,8 +39,6 @@
 		 **  ray (should be between 0 and 5). */
 #define CKVAL 1.0 /* If absolute value of the radial velocity gate is less 
 		  ** than this value, it will not be used as a PRELIM gate. */
-#define SIGN 1 /* Sign convention: if SIGN=-1, negative radial velocity is
-	       ** towards the radar, if SIGN=1 positive value towards radar. */
 #define MAXCOUNT 10 /* Maximum number of folds. */
 #define A 6372.0 /* Radius of earth in kilometers. */
 #define PI 3.1415927 
--- pyart/correct/src/prepVolume.c
+++ pyart/correct/src/prepVolume.c
@@ -1,11 +1,24 @@
 /*
+ * Filter a velocity volume based on corresponding reflectivities.
+ *  
+ * Adapted from routines from:
+ * 
+ * UW Radial Velocity Dealiasing Algorithm
+ * Four-Dimensional Dealiasing (4DD)
+ * 
+ * Developer by Curtis N. James     20 Mar 98, 25 Jan 99
+ *
+ * See UWASHINGTON_4DD_README file for license
+ *
+ * Adapted for use in Py-ART by Jonathan J. Helmus May 2014
+ */
+
+
+/*
 **
 **  UW Radial Velocity Dealiasing Algorithm
 **  Four-Dimensional Dealiasing (4DD)
 **
-**  DESCRIPTION:
-**      This routine thresholds VR bins where reflectivity is below
-**      LOWDBZ or missing (if NODBZRMRV==1).
 **
 **  DEVELOPER:
 **	Curtis N. James 20 Mar 98
@@ -14,123 +27,92 @@
 **
 */
 #include "helpers.h"
-
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <rsl.h> /* Sweep */
 
-void prepVolume(Volume* DBZVolume, Volume* rvVolume, float missingVal) {
 
-     int currIndex, sweepIndex, i, j, DBZIndex, numRays, numBins, numDBZRays,
-       numDBZBins, numSweepsRV, numSweepsDZ;
-     /* float val; XXX See below */
-     float dzval, minpossDBZ = -50.0;
-     int DBZGateSize = 0, DBZRangeBin1 = 0, DBZfactor, limit;
-     int rvGateSize = 0, rvRangeBin1 = 0, remainder;
-     div_t ratioGateSize;
-     
-     numSweepsRV = rvVolume->h.nsweeps;
-     numSweepsDZ = DBZVolume->h.nsweeps;
-     if (numSweepsRV!=numSweepsDZ) {
-       return;
-     } else {
-       for (sweepIndex=0;sweepIndex<numSweepsRV;sweepIndex++) {
-	 numRays = rvVolume->sweep[sweepIndex]->h.nrays;
-	 numBins = rvVolume->sweep[sweepIndex]->ray[0]->h.nbins;
-	 if (DBZVolume!=NULL) {
-	   numDBZRays = DBZVolume->sweep[sweepIndex]->h.nrays;
-	   numDBZBins = DBZVolume->sweep[sweepIndex]->ray[0]->h.nbins;
-	 }
-     
-	 /* Get the bin geometry for both DBZ and rv: */
+/* Test if sweeps are compatible, 0 for no, 1 for yet */
+static int sweeps_compatible(Sweep *sweep1, Sweep *sweep2)
+{
+    /* number of rays in the sweep must be the same */
+    if (sweep1->h.nrays != sweep2->h.nrays)
+        return -1;
+    /* gates must begin at same location */
+    if (sweep1->ray[0]->h.range_bin1 != sweep2->ray[0]->h.range_bin1)
+        return -1;
+    /* neither gate size can be zero */
+    if (sweep1->ray[0]->h.gate_size == 0)
+        return -1;
+    if (sweep2->ray[0]->h.gate_size == 0)
+        return -1;
+    return 1;
+}
 
-	 if (VERBOSE) printf(" ...preparing sweep...\n");
-	 if (DBZVolume!=NULL) {
-	   DBZGateSize=DBZVolume->sweep[sweepIndex]->ray[0]->
-	     h.gate_size;
-	   DBZRangeBin1=DBZVolume->sweep[sweepIndex]->ray[0]->
-	     h.range_bin1;
-	 }
-	 rvGateSize=rvVolume->sweep[sweepIndex]->ray[0]->
-	   h.gate_size;
-	 rvRangeBin1=rvVolume->sweep[sweepIndex]->ray[0]->
-	   h.range_bin1;
+/*
+ *      This routine thresholds VR bins where reflectivity is below
+ *      lowdbz or missing (if nodbzrmrv==1).
+* #define LOWDBZ 0.0 All VR bins with DZ values less than LOWDBZ will be
+		     **  deleted.
+#define HIGHDBZ 80.0 All bins with DZ values greater than HIGHDBZ will
+		     ** be deleted.
+#define NODBZRMRV 0 If NODBZRMRV==1, all VR bins with DZ values missing
+		    ** will be deleted.
 
-	 /* Terminate routine if the locations of the first range bins
-	 ** in DBZ and rv do not coincide. */
-       
-	 if ((DBZRangeBin1!=rvRangeBin1 || numDBZRays!=numRays) &&
-             DBZVolume!=NULL) {
-	   return;
-	 }
-	 /* Compare the bin geometry between DBZ and rv to know which
-	 ** DBZ bin(s) corresponds spatially to the rv bin(s)*/
-       
-	 if (DBZVolume!=NULL) {
-	   ratioGateSize=div(rvGateSize,DBZGateSize);
-	   remainder=ratioGateSize.rem;
-	   if (remainder!=0) {
-	     return;
-	   } else if (remainder==0 && rvGateSize!=0 && DBZGateSize !=0) {
-	     DBZfactor=ratioGateSize.quot;
-	   } else {
-	     return;
-	   }
-	 }
 
-	 /* Erase the first DELNUM bins of DBZ and radial velocity, as per
-	 **  communication with Peter Hildebrand. */
-	 for (currIndex=0;currIndex<numRays;currIndex++) {
-	   for (i = 0; i < DELNUM; i++) {
-	     rvVolume->sweep[sweepIndex]->ray[currIndex]->range[i]=
-	       (unsigned short) (missingVal);
-	     if (DBZVolume!=NULL) {
-	       for (j=0; j<DBZfactor; j++) {	       
-		 DBZIndex=i*DBZfactor+j;
-		 DBZVolume->sweep[sweepIndex]->ray[currIndex]->range[DBZIndex]=
-		   (unsigned short) (missingVal);
-	       }
-	     }
-	   }
-	 }
-	 
-	 /* Now that we know the bin geometry, we assign missingVal to any
-	 ** velocity bins where the reflectivity is missing (if NODBZRMRV==1)
-	 ** or outside LOWDBZ and HIGHDBZ. */
-	 
+*/
+void prepVolume(Volume* ref_volume, Volume* vel_volume, float missingVal) {
 
-	 limit=numBins;
-	 for (currIndex=0; currIndex<numRays; currIndex++) {
-	   for (i=DELNUM; i<limit; i++) {
- 	     if (DBZVolume!=NULL) {
-           /* XXX This is broken since the code is trying to use val as a 
-            * pointer
-            */
-	       /* val=(float) rvVolume->sweep[sweepIndex]->ray[currIndex]
-		 ->h.f(rvVolume->sweep[sweepIndex]->ray[currIndex]->
-		      range[i]); */
-	       for (j=0; j<DBZfactor; j++) {	       
-		 DBZIndex=i*DBZfactor+j;
-		 if (DBZIndex>=numDBZBins) {
-		   printf("Invalid bin geometry!\n");
-		   return;
-		 }
-		 dzval=(float) DBZVolume->sweep[sweepIndex]->ray[currIndex]
-		   ->h.f(DBZVolume->sweep[sweepIndex]->ray[currIndex]->
-			 range[DBZIndex]);
-		 if ((dzval>=minpossDBZ && dzval<LOWDBZ)||(((dzval<
-		     minpossDBZ)||(dzval>HIGHDBZ))&&NODBZRMRV==1)) {
-		    rvVolume->sweep[sweepIndex]->ray[currIndex]->
-		      range[i]=(unsigned short) (missingVal);
-		    /* val=missingVal; */
-		 } 
-	       }
-	     }
-	   }
-	 }
-       }
-     }
-     if (VERBOSE) printf(" ...volume prepped.\n");      
-     return;
+    /* XXX make these parameters, remove minpossDBZ variable */
+    float lowdbz = 0.0; 
+    float highdbz = 80.0;
+    int nodbzrmrv = 0;
+    float minpossDBZ = -50.0;
+    
+    int ray_index, sweep_index, i, j, ref_index, dbz_per_vel;
+    float ref;
+    div_t gate_ratio; 
+    Sweep *vel_sweep, *dbz_sweep;
+
+    if (ref_volume == NULL || vel_volume == NULL)
+        return;
+    if (vel_volume->h.nsweeps != ref_volume->h.nsweeps)
+        return;
+
+    for (sweep_index=0; sweep_index < (vel_volume->h.nsweeps); sweep_index++) {
+        vel_sweep = vel_volume->sweep[sweep_index];
+        dbz_sweep = ref_volume->sweep[sweep_index];
+        if (sweeps_compatible(vel_sweep, dbz_sweep) < 0)
+            return;
+    
+        /* Determine the number of reflectivity gates per velocity gate */
+        gate_ratio = div(vel_sweep->ray[0]->h.gate_size,
+                         dbz_sweep->ray[0]->h.gate_size);
+        if (gate_ratio.rem != 0)
+            return;     /* Gate sizes must be integer multiples */
+        else
+            dbz_per_vel = gate_ratio.quot;  
+
+        /* Assign missingVal to any velocity bins where the reflectivity
+         * is missing (if nodbzrmrv==1) or outside lowdbz and highdbz. */
+        for (ray_index=0; ray_index < (vel_sweep->h.nrays); ray_index++) {
+            for (i=0; i < (vel_sweep->ray[0]->h.nbins); i++) {
+                for (j=0; j < dbz_per_vel; j++) {
+
+                    ref_index =i * dbz_per_vel + j;
+                    if (ref_index >= (dbz_sweep->ray[0]->h.nbins))
+                        return;     /* Index out of bounds */
+
+                    ref = ray_val(dbz_sweep->ray[ray_index], ref_index);
+                    /* TODO fix this to do what is says in docs */
+                    if (ref >= minpossDBZ && ref<lowdbz) 
+                        ray_set(vel_sweep->ray[ray_index], i, missingVal);
+                    if (((ref<minpossDBZ) || (ref>highdbz)) && nodbzrmrv==1)
+                        ray_set(vel_sweep->ray[ray_index], i, missingVal);
+                }
+            }
+        }
+    }
+    return;
 }
