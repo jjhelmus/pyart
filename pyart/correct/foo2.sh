09da34af0d6bd3a0af6fc741c8ff3879a7c41972
Jonathan Helmus <jjhelmus@gmail.com>
2014-05-12 13:10:11 -0500

    Cleaning up branch prior to merging into master

--- pyart/correct/foo2.sh
+++
@@ -1,6 +0,0 @@
-# remove and rebuild the _fourdd_interface.so module
-rm  _fourdd_interface.so
-#cython _fourdd_interface.pyx
-#export RSL_PATH=/Users/jhelmus/anaconda/
-python setup.py build_ext -i
-#install_name_tool -change librsl.1.dylib /Users/jhelmus/anaconda/lib/librsl.1.dylib _fourdd_interface.so
