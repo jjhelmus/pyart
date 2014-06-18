c8b43a049bb3cfefe952cf4592496637017c9338
Jonathan Helmus <jjhelmus@gmail.com>
2014-06-18 17:34:54 -0500

    WIP: test optional fortran build

--- pyart/retrieve/setup.py
+++ pyart/retrieve/setup.py
@@ -1,17 +1,29 @@
 
 
 def configuration(parent_package='', top_path=None):
+    global config
     from numpy.distutils.misc_util import Configuration
     config = Configuration('retrieve', parent_package, top_path)
     config.add_data_dir('tests')
 
+    print "FOOOOVAR"
     # XXX make this optional
     config.add_extension(
         'echo_steiner',
-        sources=['echo_steiner.pyf', 'src/echo_steiner.f90'],
+        sources=[generate_source],
+        #sources=['echo_steiner.pyf', 'src/echo_steiner.f90'],
     )
+    #print config.have_f90c()
     return config
 
+def generate_source(ext, build_dir):
+    print "KAHHN"
+    if config.have_f90c():
+        print "HAVE"
+        return ['echo_steiner.pyf', 'src/echo_steiner.f90']
+    else:
+        print "HAVE NOT"
+        return None
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
